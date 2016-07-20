31c17115f8c70f53eae96b140776c14b9b7a9d63
Lisandro Gurman <lichun88@gmail.com>
2016-07-20 18:49:19 +0200

    Initial Commit

---
+++ para gitTests/para_gitTests.swift
@@ -0,0 +1,36 @@
+//
+//  para_gitTests.swift
+//  para gitTests
+//
+//  Created by Lisandro Gurman on 7/20/16.
+//  Copyright © 2016 Lisandro Gurman. All rights reserved.
+//
+
+import XCTest
+@testable import para_git
+
+class para_gitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
