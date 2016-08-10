1e9407a728406f7b6c6cd05e1007381bedeac0bc
Lisandro Gurman <lichun88@gmail.com>
2016-08-10 22:21:59 +0200

    primer commit

---
+++ para git/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  para git
+//
+//  Created by Lisandro Gurman on 8/10/16.
+//  Copyright © 2016 Lisandro Gurman. All rights reserved.
+//
+
+import Foundation
+
+var str = "esto es una prueba"
+Print (str)
